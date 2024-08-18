0a4aa40dc9b57675d80693cc44dabbf171615d56
funkejulianaoloko <funkejuliana98@gmail.com>
2024-08-19 00:47:35 +0100

    Add firebase

--- src/app/component/hero.tsx
+++ src/app/component/hero.tsx
@@ -1,18 +1,19 @@
 // import { Input } from "@/components/ui/input";
 // import { Label } from "@/components/ui/label";
-import { GiBreakingChain } from "react-icons/gi";
+// import { GiBreakingChain } from "react-icons/gi";
 import { Button } from "@/components/ui/button";
 import LoginAccount from "./login";
 // import SignUpAccount from "./signup";
 // import LoginAccount from "./login";
 export default function Hero() {
   return (
-    <div className="h-screen   border border-gray-50 border-dashed min-h-min py-8 item-center mt-10 relative text-center">
-      <GiBreakingChain className=" absolute left-1 text-9xl font-bold text-zinc-100 blur-sm" />
+    <div className="h-screen   border bg-gradient-to-r from-cyan-500 to-blue-500 border-dashed min-h-min py-8 item-center mt-10 relative text-center">
+      {/* <GiBreakingChain className=" absolute left-1 text-9xl font-bold text-zinc-100 blur-sm" /> */}
       <p className="max-w-fit  text-4xl font-bold py-3 w-96 mt-20  mx-auto leading-snug text-zinc-100">
         Make that social link shorter. Super fast!
       </p>
-      <p className="text-xs max-w-fit font-thin text-slate-200 py-6 px-6 w-96 mx-auto">
+      <p className=" max-w-fit
+      text-xl text-slate-200 py-6 px-6 w-96 mx-auto">
         Say good bye to long, cumbersome URLs and hello to a simpler, sleeker
         way to shorten, share and manage your links.
       </p>
